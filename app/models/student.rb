77cc906e4336547c4ab6a1820c3ea49993a10b48
Steve Kinney <hello@stevekinney.net>
2014-09-21 12:51:49 -0600

    Add loan information

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,22 +1,30 @@
 class Student < ActiveRecord::Base
-  
+
   before_validation(on: :create) do
     self.access_code = rand.to_s[2..7]
   end
-  
+
   validates :name, presence: true
   validates :access_code, presence: true
   validates :teacher_email, format: {
-    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, 
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
     message: "must be a valid email address."
   }
-  
+
   def eligible_for_pre_survey?
     pre_survey_score.nil? && post_survey_score.nil?
   end
-  
+
   def eligible_for_post_survey?
     pre_survey_score && post_survey_score.nil?
   end
-  
+
+  def eligible_for_loan_with_score(score)
+    if pre_survey_score && post_survey_score
+      pre_survey_score > score || post_survey_score > score
+    else
+      pre_survey_score > score
+    end
+  end
+
 end
